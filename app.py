7b985aed3676b42483d72a6c17ef02cc245b7234
Casper van Genderingen <c.vangenderingen@hotmail.com>
2023-01-06 13:20:22 +0100

    WIP: send to group

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    """Return a basic message"""
-    return "<p>Hello, World!</p>"
+@app.route("/send/groups/<int:group_id>")
+def send_group(group_id):
+    """Send message to all students in one group"""
+    return f'Group: {group_id}'
+
+
+@app.route("/send/locations/<int:location_id>")
+def send_location(location_id):
+    """Send message to all students from all the groups of the location"""
+    return f'Location: {location_id}'
